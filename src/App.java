dcf1d9e7b2d780bf1cf495c1456699ee2df22b68
Mrtc101 <martincogo4@gmail.com>
2024-09-05 19:07:41 -0300

    Setuping

---
+++ src/App.java
@@ -0,0 +1,14 @@
+package project;
+
+import project.Datacenter.DatacenterBrokerPSO;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    public static void main(){
+        DatacenterBrokerPSO a = new DatacenterBrokerPSO()
+    }
+}
+
+
