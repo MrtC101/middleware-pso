19d4f18e4d8cf85fcccef372d7bd903d8fbd6491
Mrtc101 <martincogo4@gmail.com>
2024-09-05 22:04:02 -0300

    new inheritance

--- src/main/java/project/Datacenter/DatacenterBrokerPSO.java
+++ src/main/java/project/Datacenter/DatacenterBrokerPSO.java
@@ -1,27 +1,42 @@
 package project.Datacenter;
 
-import org.cloudsimplus.brokers.DatacenterBroker;
-import org.cloudsimplus.brokers.DatacenterBrokerSimple;
+import org.cloudsimplus.brokers.DatacenterBrokerAbstract;
 import org.cloudsimplus.cloudlets.Cloudlet;
-import org.cloudsimplus.core.CloudSimPlus;
+import org.cloudsimplus.core.*;
 import org.cloudsimplus.datacenters.Datacenter;
 import org.cloudsimplus.vms.Vm;
 
+
 /**
- * This class based on a {@link DatacenterBroker} an implementation of a PSO based algorithm for
- * cloudlet assignment.
+ * This class based on a {@link DatacenterBrokerAbstract} an implementation of a PSO based
+ * algorithm for cloudlet assignment.
  *
  * @author Cogo Martín
  */
-public class DatacenterBrokerPSO extends DatacenterBrokerSimple {
+public class DatacenterBrokerPSO extends DatacenterBrokerAbstract {
 
-    public DatacenterBrokerPSO(final CloudSimPlus simulation){
-        super(simulation);
+    public DatacenterBrokerPSO(CloudSimPlus simulation,String name){
+        super(simulation, name);
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented constructor");
     }
 
-    public static void main(String[] args){
-        final CloudSimPlus simulation = new CloudSimPlus();
+    public static void main(String[] args) {
         System.out.println("Running...");
-        new DatacenterBrokerPSO(simulation);
+        CloudSimPlus simulation = new CloudSimPlus();
+        new DatacenterBrokerPSO(simulation,"");
+    }
+    
+    @Override
+    protected Datacenter defaultDatacenterMapper(Datacenter lastDatacenter, Vm vm) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'defaultDatacenterMapper'");
+    }
+
+    @Override
+    protected Vm defaultVmMapper(Cloudlet cloudlet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'defaultVmMapper'");
     }
 }
+
