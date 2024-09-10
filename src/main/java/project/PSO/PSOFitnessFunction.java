a6e29a5806dd1ac51eac4a67bef85b99ba59dcd1
Mrtc101 <martincogo4@gmail.com>
2024-09-10 20:44:01 -0300

    Ready for debbugging PSOFitnessFunction

--- src/main/java/project/PSO/PSOFitnessFunction.java
+++ src/main/java/project/PSO/PSOFitnessFunction.java
@@ -7,29 +7,64 @@ import org.cloudsimplus.vms.Vm;
 
 public class PSOFitnessFunction extends FitnessFunction {
 
-    private double[][] costMatrix;
+    private ArrayList<Cloudlet> cloudletList;
+    private ArrayList<Vm> vmList;
+
 
     public PSOFitnessFunction(ArrayList<Cloudlet> cloudletList, ArrayList<Vm> vmList) {
         super(false);
-        costMatrix = buildCostMatrix(cloudletList, vmList);
+        this.cloudletList = cloudletList;
+        this.vmList = vmList;
     }
 
+    /**
+     * Objective function taken from BungaMapetu[2019]
+     */
     @Override
     public double evaluate(double[] position) {
-        double totalCost = 0;
-        for (int tskId = 0; tskId < position.length; tskId++) {
-            int vmId = (int) position[tskId];
-            totalCost += costMatrix[tskId][vmId];
-        }
+        double[] vmsCT = calcCompletionTime(position);
+        double totalCost = calcMaxCtDifference(vmsCT);
         return totalCost;
     }
 
-    private double[][] buildCostMatrix(ArrayList<Cloudlet> cloudlets, ArrayList<Vm> vms) {
-        int numCloudlets = cloudlets.size();
-        int numVms = vms.size();
-        double[][] matrix = new double[numCloudlets][numVms];
-        // TODO: develop how to fill the cost matrix.
-        return matrix;
+    private double calcMaxCtDifference(double[] vmsCT) {
+        double maxCT = 0;
+        for(int i = 0; i < vmsCT.length; i++){
+            for(int j = 0; j < vmsCT.length; j++){
+                if(i != j){
+                    double currCT = vmsCT[i] - vmsCT[j];
+                    if(currCT > maxCT){
+                        maxCT = currCT;
+                    }
+                }
+            }
+        }
+        return maxCT;
     }
 
+    private double[] calcCompletionTime(double[] position) {
+        double[] CTs = new double[vmList.size()];
+        
+        for (int taskId = 0; taskId < position.length; taskId++) {
+            int vmId = (int) position[taskId];
+            
+            if (vmId < 0 || vmId >= vmList.size()) {
+                throw new IllegalArgumentException("VM ID out of bounds: " + vmId);
+            }
+            if (taskId < 0 || taskId >= cloudletList.size()) {
+                throw new IllegalArgumentException("Task ID out of bounds: " + taskId);
+            }
+    
+            Vm currVm = vmList.get(vmId);
+            Cloudlet currTask = cloudletList.get(taskId);
+            
+            if (currVm.getPesNumber() == 0) {
+                throw new IllegalArgumentException(vmId + " VM have 0 PEs.");
+            }
+    
+            CTs[vmId] += currTask.getLength() / (currVm.getPesNumber() * currVm.getMips());
+        }
+        
+        return CTs;
+    }
 }
