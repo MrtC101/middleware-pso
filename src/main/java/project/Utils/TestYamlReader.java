f731fbc06b3623d3e55d8356813bcb716c541997
juanm4morales <juanm4morales@gmail.com>
2024-10-14 16:11:59 -0300

    Removed TestYamlReader

--- src/main/java/project/Utils/TestYamlReader.java
+++
@@ -1,9 +0,0 @@
-package project.Utils;
-
-public class TestYamlReader {
-    
-    public static void main(String[] args) {
-        YamlReader.readConfig("./src/main/java/project/Utils/y.yaml");
-
-    }
-}
