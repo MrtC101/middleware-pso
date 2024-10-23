8435fa73e59d0435ca9501aa97bd35fd81abc6ba
juanm4morales <juanm4morales@gmail.com>
2024-10-23 13:35:10 -0300

    Yaml file integration Example test

--- src/main/java/project/Examples/Test.java
+++ src/main/java/project/Examples/Test.java
@@ -7,8 +7,20 @@ import project.Utils.YamlReader;
 
 public class Test {
     public static void main(String[] args) {
+        /*
         DatacenterConfig datacenterConfig = YamlReader.readConfig("./src/main/java/project/Examples/y_test.yaml");
         SimGenerator simGenerator = new SimGenerator(datacenterConfig);
         simGenerator.generateSimulation();
+         */
+        
+
+       
+        String costsFile = "./src/main/java/project/Examples/costs.yaml";
+        String hostsFile = "./src/main/java/project/Examples/hostsConfig.yaml";
+        String vmsFile = "./src/main/java/project/Examples/vmsConfig.yaml";
+        String tasksFile = "./src/main/java/project/Examples/tasksConfig.yaml";
+        DatacenterConfig datacenterConfig2 = YamlReader.readConfig(costsFile, hostsFile, vmsFile, tasksFile);
+        SimGenerator simGenerator2 = new SimGenerator(datacenterConfig2);
+        simGenerator2.generateSimulation();
     }
 }
